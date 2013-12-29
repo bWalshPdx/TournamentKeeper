13f95592edc9efe0d5b96b43e42f493a60cd99dc
bWalshPdx <bwalshpdx@gmail.com>
2013-12-28 20:37:24 -0800

    Weight class sort complete
    code and Initial Test complete

---
+++ Keeper.Service.Tests/Timer_Tests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Keeper.Service.Tests
+{
+
+    public class MockStuff
+    {
+        enum Rank { White = 1, Blue = 2, Purple = 3, Brown = 4, Black = 5 };
+
+
+        Random rand = new Random();
+        public Competitor GenerateCompetitor()
+        {
+            Rank Belts;
+
+            Competitor output = new Competitor()
+                                    {
+                                        Rank = Enum.Parse(typeof(Rank), rand.Next(1, 5).ToString()).ToString(),
+                                        FirstName = getFirstName(),
+                                        LastName = getLastName(),
+                                        Team = getTeamName(),
+                                        Weight = rand.Next(100,270),
+                                        IsMale = rand.Next(0,9) < 3
+                                    };
+            return output;
+        }
+
+
+        public Stack<Competitor> getFakeCompetitors(int count)
+        {
+            var output = new Stack<Competitor>();
+
+            MockStuff ms = new MockStuff();
+            for (int i = 0; i < count; i++)
+            {
+                output.Push(ms.GenerateCompetitor());
+            }
+            return output;
+        }
+
+        public string getFirstName()
+        {
+
+            List<string> firstName = new List<string>()
+                                         {
+                                             "Kapow",
+                                             "Bam Bam",
+                                             "Conkie",
+                                             "Jennifer",
+                                             "Amsterdam",
+                                             "Ratsie",
+                                             "Lemar",
+                                             "Webly",
+                                             "Brian",
+                                             "JamFace",
+                                             "Barbara",
+                                             "Paul",
+                                             "Maggie",
+                                             "Annie"
+                                         };
+
+            return firstName[rand.Next(0, firstName.Count() - 1)];
+        }
+
+        public string getLastName()
+        {
+            List<string> lastName = new List<string>()
+                                         {
+                                             "Walsh",
+                                             "Bowles",
+                                             "Walker",
+                                             "Lyon",
+                                             "Wrekk",
+                                             "Lamar",
+                                             "Kuo",
+                                             "Edwards",
+                                             "Walrath",
+                                             "Johnson",
+                                             "DeLooze",
+                                             "Debruge",
+                                             "LouLou",
+                                             "Talleo"
+                                         };
+
+            return lastName[rand.Next(0, lastName.Count() - 1)];
+        }
+
+        public string getTeamName()
+        {
+
+            List<string> teamName = new List<string>()
+                                         {
+                                             "Rickson Gracie",
+                                             "NW Martial Arts",
+                                             "Impact Jiu Jitsu",
+                                             "Straightblast Gym",
+                                             "Ralph Gracie Portland",
+                                             "Knuckle Up",
+                                             "Team Lovato",
+                                             "Ribiero Jiu Jitsu",
+                                             "Brazillian Top Team",
+                                             "Ralph Machio Jiu Jitsu",
+                                             "Team Miyagi"
+                                         };
+
+            return teamName[rand.Next(0, teamName.Count() - 1)];
+        }
+
+    }
+
+    [TestFixture]
+    class Timer_Tests
+    {
+        [Test]
+        public void getCompetitors_VerifyOutput()
+        {
+            
+
+            MockStuff ms = new MockStuff();
+            for (int i = 0; i < 100; i++)
+            {
+                var comp = ms.GenerateCompetitor();
+                string output = @"Rank: {0}
+FirstName = {1},
+LastName = {2},
+Team = {3},
+Weight = {4},
+Gender = {5}
+";
+                Console.WriteLine(output,comp.Rank,comp.FirstName, comp.LastName,comp.Team, comp.Weight, comp.IsMale == true ? "Male": "Female");
+                
+            }
+        }
+
+
+        [Test]
+        //TODO: Rewrite this test to document why it is failing:
+        public void sortByWeight_InitialTest()
+        {
+            int numberOfCompetitors = 1000;
+            MockStuff ms = new MockStuff();
+            Sort s = new Sort();
+            var fakeCompetitors = ms.getFakeCompetitors(numberOfCompetitors);
+            
+            List<double> weightBrackets = new List<double>()
+                                           {
+                                               125.5,
+                                               141,
+                                               154,
+                                               167.5,
+                                               181,
+                                               194.5,
+                                               207.5,
+                                               221
+                                           };
+
+            var output = s.seperateWeight(fakeCompetitors, weightBrackets).ToList();
+
+
+            for (int i = 0; i <= weightBrackets.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("----------0lbs - {0}lbs---------", weightBrackets[i]);
+                }
+                else if (i == weightBrackets.Count)
+                {
+                    Console.WriteLine("----------{0}lbs - Infinity -----------", weightBrackets[i - 1]);
+                }
+                else
+                {
+                    Console.WriteLine("----------{0}lbs - {1}lbs---------", weightBrackets[i - 1], weightBrackets[i]);
+                }
+
+                
+
+                foreach (var comp in output[i])
+                {
+                    Console.WriteLine("Weight: {0}", comp.Weight);
+                    if (i == 0)
+                    {
+                        Assert.IsTrue(comp.Weight < weightBrackets[i]);
+                    }
+                    else if (i == weightBrackets.Count)
+                    {
+                        Assert.IsTrue(weightBrackets[i - 1] <= comp.Weight);
+                    }
+                    else
+                    {
+                        Assert.IsTrue(weightBrackets[i - 1] <= comp.Weight && comp.Weight < weightBrackets[i]);
+                    }
+                }
+
+            }
+        }
+   
+
+
+
+
+    }
+}
---
+++ Keeper.Service/Keeper.Service/Sort.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.Service
+{
+    public class Sort
+    {
+        public List<Bout> InitalSort(Stack<Competitor> competitors)
+        {
+            //by Wieght then teams
+            return new List<Bout>();
+        }
+
+        public List<Bout> NextSort(Stack<Competitor> competitors)
+        {
+            //by Teams   
+            return new List<Bout>();
+        }
+
+        //http://www.londoninternationalbjj.com/files/65971007.jpg
+        //TODO: Converting from list to stack is jumbling the sort:
+        public IEnumerable<Stack<Competitor>> seperateWeight(Stack<Competitor> competitors, List<double> weightBrackets)
+        {
+            //Order competitors in ascending order:
+            Stack<Competitor> sortedComps = new Stack<Competitor>(competitors.OrderByDescending(a => a.Weight).ToList());
+
+            //TODO: Order the stack without the dumb temp list:
+            
+            //pinch off list at the top of each bracket
+            Stack<Competitor> current = new Stack<Competitor>();
+
+            foreach (var weightBracket in weightBrackets)
+            {
+                current = new Stack<Competitor>();
+
+                while (true)
+                {
+                    if (sortedComps.Peek().Weight < weightBracket && competitors.Count > 0)
+                    {
+                        current.Push(sortedComps.Pop());
+                    }
+                    else
+                    {
+                        yield return current;
+                        break;
+                    }
+                }
+            }
+
+            current = new Stack<Competitor>();
+            while (sortedComps.Count > 0)
+                current.Push(sortedComps.Pop());
+
+            yield return current;
+        }
+
+        public List<Bout> seperateTeams(Stack<Competitor> competitors)
+        {
+            //Seperate teams in merge sort style, teams are grouped, pull each team from the "head" of each group.
+            return new List<Bout>();
+
+        }
+    }
+
+    public class Bout
+    {
+        public List<CombantantInfo> Combabtants;
+        public int TimeOfBout;
+        public CombantantInfo Winner;
+        public Timer Timer;
+
+
+
+        public Bout(IEnumerable<Competitor> competitors)
+        {
+            Combabtants = competitors.Select(c => (CombantantInfo) c).ToList();
+        }
+
+
+        
+    }
+
+    public class Competitor
+    {
+        public string Team;
+        public string FirstName;
+        public string LastName;
+        public string Rank;
+        public int Weight;
+        public bool IsMale;
+
+    }
+
+    public class Team
+    {
+        public string Name;
+        public int TeamId;
+
+
+    }
+
+    //TODO: Add all the submissions:
+    public enum Submission {Armbar, Kneebar, AnkleLock, Choke, Kimuara, WristLock}
+
+    public class CombantantInfo : Competitor
+    {
+        public int Points;
+        public bool isGreenBelt;
+        public bool? IsWinner;
+        public Submission? WinningSubmission;
+    }
+
+}
